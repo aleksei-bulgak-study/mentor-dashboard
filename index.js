e701d00dbaeb63ab33c509ac716e64673e0d5fd6
aleksei-bulgak-study <bulgak.aleksei@yandex.ru>
2019-02-24 11:40:29 +0300

    fix: add addtional loggin

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const asyncHandler = require('./asyncHandler');
 const PORT = process.env.PORT;
 const CLIENT_ID = process.env.CLIENT_ID;
 const SECRET_CLIENT_ID = process.env.SECRET_CLIENT_ID;
+console.log(PORT);
+console.log(CLIENT_ID);
+console.log(SECRET_CLIENT_ID);
 
 const accessToken = 'https://github.com/login/oauth/access_token';
 const user = 'https://api.github.com/user';
@@ -25,12 +28,19 @@ app.get('/login/:clientId',
   asyncHandler(async (req, res) => {
     const clientId = req.params.clientId;
     const url = `${accessToken}?client_id=${CLIENT_ID}&client_secret=${SECRET_CLIENT_ID}&code=${clientId}`;
-
+    console.log(url);
     const data = await fetch(url)
-      .then(response => response.text())
+      .then(response => {
+        console.log(response);
+        return response.text()
+      })
       .then(data => fetch(`${user}?${data}`))
-      .then(response => response.json())
+      .then(response => {
+        console.log(response);
+        return response.json()
+      })
       .then(data => data.login);
+    console.log(data);
     res.send(data);
   })
 );
