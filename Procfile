aa1fc94e56ebdcb49217bd7e59b590a811f40d04
aleksei-bulgak-study <bulgak.aleksei@yandex.ru>
2019-02-24 11:17:05 +0300

    configuation for server

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web node dashboard/server/index.js
+web node index.js
--- dashboard/scripts/src/store/index.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-
-module.exports.storeFile = (data, file, dir = '') => {
-  const pathToFile = path.join(__dirname, dir, file);
-  const dataJson = JSON.stringify(data);
-  fs.writeFile(pathToFile, dataJson, error => error && process.stderr.write(error));
-};
