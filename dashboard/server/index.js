9a3fd8bd8f4f00a77ed5dfb31aadfc556332cab4
aleksei-bulgak-study <bulgak.aleksei@yandex.ru>
2019-02-24 11:10:16 +0300

    feat: update server arguments

--- dashboard/server/index.js
+++ dashboard/server/index.js
@@ -4,8 +4,13 @@ const bodyParser = require('body-parser');
 const fetch = require('node-fetch');
 const asyncHandler = require('./asyncHandler');
 
-// const PORT = process.env.PORT;
-const PORT = 8080;
+const PORT = process.env.PORT;
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_ID = process.env.SECRET_CLIENT_ID;
+
+const accessToken = 'https://github.com/login/oauth/access_token';
+const user = 'https://api.github.com/user';
+
 const app = express();
 app.use(bodyParser.json());
 
@@ -18,21 +23,14 @@ app.use((req, res, next) => {
 
 app.get('/login/:clientId',
   asyncHandler(async (req, res) => {
-    const uuid1 = 'bcf0a6cf1918c0481c37';
-    const uuid2 = '660e41c020e85e8ed47084c12bd874e47ed656dd';
-    const accessToken= 'https://github.com/login/oauth/access_token';
     const clientId = req.params.clientId;
-    const user= 'https://api.github.com/user';
-    const url = `${accessToken}?client_id=${uuid1}&client_secret=${uuid2}&code=${clientId}`;
+    const url = `${accessToken}?client_id=${CLIENT_ID}&client_secret=${CLIENT_ID}&code=${clientId}`;
 
     const data = await fetch(url)
       .then(response => response.text())
       .then(data => fetch(`${user}?${data}`))
       .then(response => response.json())
-      .then(data => {
-        console.log(data)
-        return data.login
-      });
+      .then(data => data.login);
     res.send(data);
   })
 );
